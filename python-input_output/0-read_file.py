afff58df51840260005a5657c4bdbb3b6b6c9de3
Waliyat <w.badmus@alustudent.com>
2025-07-20 11:37:20 -0700

    read

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""This module defines a function to read a text file (UTF8) and print its content to stdout."""
+"""This module defines a function to read a text 
+file (UTF8) and print its content to stdout."""
 
 
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout."""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
-
