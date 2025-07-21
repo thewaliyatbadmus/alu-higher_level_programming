c42f9decea4ac272bfdda804337fd5d6de603b01
Waliyat <w.badmus@alustudent.com>
2025-07-21 04:50:59 -0700

    write file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-"""Module that defines a function to write a string to a text file."""
+"""This module defines a function that writes a string to a UTF8 text file."""
+
 
 def write_file(filename="", text=""):
-    """Writes a string to a text file (UTF8) and returns the number of characters written."""
-    with open(filename, "w", encoding="utf-8") as f:
+    """
+    Writes a string to a text file (UTF8) and returns
+    the number of characters written.
+    """
+    with open(filename, mode="w", encoding="utf-8") as f:
         return f.write(text)
