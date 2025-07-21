cdcdc5a8da387f4d7515365cacf06990db492818
Waliyat <w.badmus@alustudent.com>
2025-07-21 05:37:44 -0700

    4upd

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""This module defines a function that returns an object by decoding a JSON string."""
-
+"""This module defines a function that returns
+an object by decoding a JSON string.
+"""
 
 import json
 
 
 def from_json_string(my_str):
-    """Returns an object (Python data structure) represented by a JSON string."""
+    """
+    Returns an object (Python data structure)
+    represented by a JSON string.
+    """
     return json.loads(my_str)
