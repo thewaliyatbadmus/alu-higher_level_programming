6026a77dd797087a5e8e0529ae8c5e2217d85491
Waliyat <w.badmus@alustudent.com>
2025-07-21 05:35:33 -0700

    part4

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a function that returns an object by decoding a JSON string."""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """Returns an object (Python data structure) represented by a JSON string."""
+    return json.loads(my_str)
