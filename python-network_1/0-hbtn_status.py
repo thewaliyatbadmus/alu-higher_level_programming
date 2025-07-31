1f426252dea2112c9aca846aaad091bee615b101
Waliyat <w.badmus@alustudent.com>
2025-07-31 06:47:26 -0700

    uod

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""Fetches https://alu-intranet.hbtn.io/status."""
+"""Fetches a URL status page."""
 import urllib.request
+import sys
 
 if __name__ == "__main__":
     url = "https://alu-intranet.hbtn.io/status"
+    if len(sys.argv) > 1:
+        url = sys.argv[1]
+
     req = urllib.request.Request(url, headers={"User-Agent": "Mozilla/5.0"})
     with urllib.request.urlopen(req) as response:
         body = response.read()
