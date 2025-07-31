8416bcdd65b4ad5edbd2602f8fe8165b7d5c9c8e
Waliyat <w.badmus@alustudent.com>
2025-07-31 06:52:12 -0700

    upd

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-"""Fetches a URL status page."""
+"""Fetches https://alu-intranet.hbtn.io/status using urllib only."""
 import urllib.request
-import sys
 
-if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
-    if len(sys.argv) > 1:
-        url = sys.argv[1]
-
-    req = urllib.request.Request(url, headers={"User-Agent": "Mozilla/5.0"})
-    with urllib.request.urlopen(req) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+with urllib.request.urlopen("https://alu-intranet.hbtn.io/status") as response:
+    body = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode("utf-8")))
